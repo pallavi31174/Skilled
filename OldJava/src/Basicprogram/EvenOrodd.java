9ee90c7d770c2ad8c61279fb9f50128c71c6772a
Pallavi kolla <pallavi31174>
2025-05-27 17:13:43 +0100

    Even

---
+++ OldJava/src/Basicprogram/EvenOrodd.java
@@ -0,0 +1,19 @@
+package Basicprogram;
+
+public class EvenOrodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int n=12;
+      if(n % 2 == 0)
+      {
+    	  System.out.println("n is even number");
+      }
+      else
+      {
+    	  System.out.println("n is odd number");
+      }
+	}
+
+
+}
